e5b10b4c1731c1b15f4db348456f2413f13b4d5a
David Rangel <rangelography@gmail.com>
2016-12-08 17:00:55 -0600

    added more number buttons to js

--- 2.4-calculator/main.js
+++ 2.4-calculator/main.js
@@ -9,11 +9,34 @@
       //   to empty
 // first button: click button "7" and have "7" appear in calculatorOutput
 document.addEventListener("DOMContentLoaded", function(){
-
+  // make vars to identify buttons and where to display output
   var nameButton7Element = document.querySelector("[data-js='button7']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   // add eventListener for the button click
   nameButton7Element.addEventListener("click", function(){
     outputElement.textContent = 7;
+  });
+  var nameButton8Element = document.querySelector("[data-js='button8']");
+  var outputElement = document.querySelector("[data-js='outputDisplay']");
+  // add eventListener for the button click
+  nameButton8Element.addEventListener("click", function(){
+    outputElement.textContent = 8;
+  });
+  var nameButton9Element = document.querySelector("[data-js='button9']");
+  var outputElement = document.querySelector("[data-js='outputDisplay']");
+  // add eventListener for the button click
+  nameButton9Element.addEventListener("click", function(){
+    outputElement.textContent = 9;
+  });
+  var nameButton4Element = document.querySelector("[data-js='button4']");
+  var outputElement = document.querySelector("[data-js='outputDisplay']");
+  // add eventListener for the button click
+  nameButton4Element.addEventListener("click", function(){
+    outputElement.textContent = 4;
+  });
+  var nameButton5Element = document.querySelector("[data-js='button5']");
+  var outputElement = document.querySelector("[data-js='outputDisplay']");
+  nameButton5Element.addEventListener("click", function(){
+    outputElement.textContent = 5;
   })
 })
