e59f7add7ef56dafcffe17adfcb2c2c3ff9270ed
David Rangel <rangelography@gmail.com>
2016-12-09 16:07:11 -0600

    added for loop for numeric buttons in calc

--- 2.4-calculator/main.js
+++ 2.4-calculator/main.js
@@ -10,64 +10,18 @@
 // first button: click button "7" and have "7" appear in calculatorOutput
 document.addEventListener("DOMContentLoaded", function(){
   // make vars to identify buttons and where to display output
-  var nameButton7Element = document.querySelector("[data-js='button7']");
+  var nameButtonElements = document.querySelectorAll("[data-js='buttonNum']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   // add eventListener for the button click
-  nameButton7Element.addEventListener("click", function(){
-    outputElement.textContent += 7;
-  });
-  var nameButton8Element = document.querySelector("[data-js='button8']");
-  var outputElement = document.querySelector("[data-js='outputDisplay']");
-  // add eventListener for the button click
-  nameButton8Element.addEventListener("click", function(){
-    outputElement.textContent += 8;
-  });
-  var nameButton9Element = document.querySelector("[data-js='button9']");
-  var outputElement = document.querySelector("[data-js='outputDisplay']");
-  // add eventListener for the button click
-  nameButton9Element.addEventListener("click", function(){
-    outputElement.textContent += 9;
-  });
-  var nameButton4Element = document.querySelector("[data-js='button4']");
-  var outputElement = document.querySelector("[data-js='outputDisplay']");
-  // add eventListener for the button click
-  nameButton4Element.addEventListener("click", function(){
-    outputElement.textContent += 4;
-  });
-  var nameButton5Element = document.querySelector("[data-js='button5']");
-  var outputElement = document.querySelector("[data-js='outputDisplay']");
-  nameButton5Element.addEventListener("click", function(){
-    outputElement.textContent += 5;
-  });
-  var nameButton6Element = document.querySelector("[data-js='button6']");
-  var outputElement = document.querySelector("[data-js='outputDisplay']");
-    nameButton6Element.addEventListener("click", function(){
-    outputElement.textContent += 6;
-  });
-  var nameButton1Element = document.querySelector("[data-js='button1']");
-  var outputElement = document.querySelector("[data-js='outputDisplay']");
-    nameButton1Element.addEventListener("click", function(){
-    outputElement.textContent += 1;
-  });
-  var nameButton2Element = document.querySelector("[data-js='button2']");
-  var outputElement = document.querySelector("[data-js='outputDisplay']");
-    nameButton2Element.addEventListener("click", function(){
-    outputElement.textContent += 2;
-  });
-  var nameButton3Element = document.querySelector("[data-js='button3']");
-  var outputElement = document.querySelector("[data-js='outputDisplay']");
-    nameButton3Element.addEventListener("click", function(){
-    outputElement.textContent += 3;
-  });
+  for(i = 0; i < nameButtonElements.length; i++){
+    nameButtonElements[i].addEventListener("click", function(){
+      outputElement.textContent += this.textContent;
+    });
+  }
   var nameButtonDecimalElement = document.querySelector("[data-js='buttonDecimal']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
     nameButtonDecimalElement.addEventListener("click", function(){
     outputElement.textContent += ".";
-  });
-  var nameButton0Element = document.querySelector("[data-js='button0']");
-  var outputElement = document.querySelector("[data-js='outputDisplay']");
-    nameButton0Element.addEventListener("click", function(){
-    outputElement.textContent += 0;
   });
   // make the clear all key:
   var nameButtonClearElement =  document.querySelector("[data-js='buttonClear']");
