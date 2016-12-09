0c071c26f6b4c185b8c1a4b6cb4a733fb41fbecc
David Rangel <rangelography@gmail.com>
2016-12-08 22:52:21 -0600

    fixed number buttons to concat

--- 2.4-calculator/main.js
+++ 2.4-calculator/main.js
@@ -14,55 +14,55 @@ document.addEventListener("DOMContentLoaded", function(){
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   // add eventListener for the button click
   nameButton7Element.addEventListener("click", function(){
-    outputElement.textContent = 7;
+    outputElement.textContent += 7;
   });
   var nameButton8Element = document.querySelector("[data-js='button8']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   // add eventListener for the button click
   nameButton8Element.addEventListener("click", function(){
-    outputElement.textContent = 8;
+    outputElement.textContent += 8;
   });
   var nameButton9Element = document.querySelector("[data-js='button9']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   // add eventListener for the button click
   nameButton9Element.addEventListener("click", function(){
-    outputElement.textContent = 9;
+    outputElement.textContent += 9;
   });
   var nameButton4Element = document.querySelector("[data-js='button4']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   // add eventListener for the button click
   nameButton4Element.addEventListener("click", function(){
-    outputElement.textContent = 4;
+    outputElement.textContent += 4;
   });
   var nameButton5Element = document.querySelector("[data-js='button5']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   nameButton5Element.addEventListener("click", function(){
-    outputElement.textContent = 5;
+    outputElement.textContent += 5;
   });
   var nameButton6Element = document.querySelector("[data-js='button6']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   nameButton6Element.addEventListener("click", function(){
-    outputElement.textContent = 6;
+    outputElement.textContent += 6;
   });
   var nameButton1Element = document.querySelector("[data-js='button1']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   nameButton1Element.addEventListener("click", function(){
-    outputElement.textContent = 1;
+    outputElement.textContent += 1;
   });
   var nameButton2Element = document.querySelector("[data-js='button2']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   nameButton2Element.addEventListener("click", function(){
-    outputElement.textContent = 2;
+    outputElement.textContent += 2;
   });
   var nameButton3Element = document.querySelector("[data-js='button3']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   nameButton3Element.addEventListener("click", function(){
-    outputElement.textContent = 3;
+    outputElement.textContent += 3;
   });
   var nameButton0Element = document.querySelector("[data-js='button0']");
   var outputElement = document.querySelector("[data-js='outputDisplay']");
   nameButton0Element.addEventListener("click", function(){
-    outputElement.textContent = 0;
+    outputElement.textContent += 0;
   });
   // make the clear all key:
   var nameButtonClearElement = document.querySelector("[data-js='buttonClear']");
@@ -70,7 +70,10 @@ document.addEventListener("DOMContentLoaded", function(){
   nameButtonClearElement.addEventListener("click", function(){
     outputElement.textContent = "";
   });
-  // Now I want to make the ouput concatenate input in the outputDisplay
-  // Next I need to display the concatenated output in the outputDisplay
-  // Then I need to redisplay that output as a new output
+  // Need to make the math functions
+  var nameButtonAddElement = document.querySelector("[data-js='buttonAdd']");
+  var outputElement = document.querySelector("[data-js='outputDisplay']");
+  nameButtonAddElement.addEventListener("click", function(){
+    outputElement.textContent += "+";
+  });
 });
