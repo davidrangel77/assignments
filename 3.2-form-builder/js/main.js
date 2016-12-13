bdd1099cb8c73f3810b25cae19cc758620e14fd8
David Rangel <rangelography@gmail.com>
2016-12-13 15:42:16 -0600

    added two loops for assignment 3.2

--- 3.2-form-builder/js/main.js
+++ 3.2-form-builder/js/main.js
@@ -4,10 +4,26 @@ var xhr = new XMLHttpRequest;
 xhr.open("GET", "http://json-data.herokuapp.com/forms");
 xhr.addEventListener("load", function(e){
   var responseData = JSON.parse(this.response);
-  console.log(responseData);
-  var formElement = document.querySelector("[data-js='form']")
-  console.log(formElement);
-  formElement.innerHTML += responseData[0];
-  });
+  var formElementsArray = document.querySelector("[data-js='form']")
+  var allInputDataHtml = "";
+  responseData.forEach(function(inputData){
+    // make each input html strings
+    var inputDataPlaceholder = "";
+    if (inputData.type === "select"){
+      inputDataPlaceholder += `<select>`;
+        inputData.options.forEach(function(options){
+          // if (i dont know what goes here) += `option goes here`
+          // else (i dont know what goes here) += `option2 goes here`
+        })
+    }else {
+      inputDataPlaceholder += `<input type="${inputData.type}"
+        id="${inputData.id}" icon="${inputData.icon}">`;
+      inputDataPlaceholder += `<label> for="${inputData.label}"</label>`;
+    }
+    // concat all inputs to input string to allInputData
+    allInputDataHtml += inputDataPlaceholder;
+  })
+  console.log(allInputDataHtml);
+});
 xhr.send();
 });
