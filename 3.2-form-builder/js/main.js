625658003b62b1bad381203f99cd7dc1397e3b61
David Rangel <rangelography@gmail.com>
2016-12-13 17:02:56 -0600

    finished regular mode and comments

--- 3.2-form-builder/js/main.js
+++ 3.2-form-builder/js/main.js
@@ -4,26 +4,34 @@ var xhr = new XMLHttpRequest;
 xhr.open("GET", "http://json-data.herokuapp.com/forms");
 xhr.addEventListener("load", function(e){
   var responseData = JSON.parse(this.response);
-  var formElementsArray = document.querySelector("[data-js='form']")
+  // make var to connect the data-js to the .html element
+  var formElement = document.querySelector("[data-js='form']")
+  // make placeholder for outside the first forEach loop that holds everything
   var allInputDataHtml = "";
+  // make forEach loop to loop throgh inputData to identify what "type" of input
   responseData.forEach(function(inputData){
-    // make each input html strings
+  // make another placeholder for outside the interior forEach loop
     var inputDataPlaceholder = "";
+    // make if statement to start building "select" html. That is, if the loop sees something that has a data type that equals "select" it will start the html concatenation.
     if (inputData.type === "select"){
+      // this begins the "select" html backtick IF it is the "select" data type
       inputDataPlaceholder += `<select>`;
-        inputData.options.forEach(function(options){
-          // if (i dont know what goes here) += `option goes here`
-          // else (i dont know what goes here) += `option2 goes here`
+      // starts the next loop through the "options" in the "select" data
+        inputData.options.forEach(function(optionData){
+          // continues concat'ing with the html for the "option"s in proper html
+          inputDataPlaceholder += `<option label="${optionData.label}" value="${optionData.value}"</option>`;
         })
+      // the "else" statement is for all the other inputs that ARE NOT "select"
     }else {
-      inputDataPlaceholder += `<input type="${inputData.type}"
+      inputDataPlaceholder += `<label>${inputData.label}</label>`;
+      inputDataPlaceholder += `<input class="textInput" type="${inputData.type}"
         id="${inputData.id}" icon="${inputData.icon}">`;
-      inputDataPlaceholder += `<label> for="${inputData.label}"</label>`;
     }
-    // concat all inputs to input string to allInputData
+    // concat's all inputs into the allInputData (the placeholder from above that holds everything)
     allInputDataHtml += inputDataPlaceholder;
-  })
-  console.log(allInputDataHtml);
+  });
+  // adds it all to the html.
+formElement.innerHTML += allInputDataHtml;
 });
 xhr.send();
 });
