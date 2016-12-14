77680e65e59a4d7394e278fe3c4a99e5c2eac5ac
David Rangel <rangelography@gmail.com>
2016-12-13 19:32:39 -0600

    worked on css styling for form page

--- 3.2-form-builder/js/main.js
+++ 3.2-form-builder/js/main.js
@@ -15,7 +15,7 @@ xhr.addEventListener("load", function(e){
     // make if statement to start building "select" html. That is, if the loop sees something that has a data type that equals "select" it will start the html concatenation.
     if (inputData.type === "select"){
       // this begins the "select" html backtick IF it is the "select" data type
-      inputDataPlaceholder += `<select>`;
+      inputDataPlaceholder += `<select class="selector">`;
       // starts the next loop through the "options" in the "select" data
         inputData.options.forEach(function(optionData){
           // continues concat'ing with the html for the "option"s in proper html
@@ -23,9 +23,10 @@ xhr.addEventListener("load", function(e){
         })
       // the "else" statement is for all the other inputs that ARE NOT "select"
     }else {
-      inputDataPlaceholder += `<label>${inputData.label}</label>`;
-      inputDataPlaceholder += `<input class="textInput" type="${inputData.type}"
+      // inputDataPlaceholder += `<label>${inputData.label}</label>`;
+      inputDataPlaceholder += `<input class="textInput" placeholder="${inputData.label}" type="${inputData.type}"
         id="${inputData.id}" icon="${inputData.icon}">`;
+        console.log(inputData.icon);
     }
     // concat's all inputs into the allInputData (the placeholder from above that holds everything)
     allInputDataHtml += inputDataPlaceholder;
