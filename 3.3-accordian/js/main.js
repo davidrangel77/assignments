5ff1b4d044479f1ae34e04aa888e95df1544e560
David Rangel <rangelography@gmail.com>
2016-12-14 14:17:35 -0600

    added main js

--- 3.3-accordian/js/main.js
+++ 3.3-accordian/js/main.js
@@ -4,9 +4,15 @@ $(function() {
   // need to make a var for the elements in the html to connect data-js
   var $articleElements = $("[data-js='article']");
   // need to use an $.on to loop through them to look for the click
-
+  $articleElements.on("click", function(e){
+    // make var to reference "$this"
+    var $clickedArticle = $(this);
   // change the class to expand on click
+  // but will do this below the .filter and .removeClass to that we don't negate the first time the articleElement is clicked
   // need to make a find class for elements with the expanded status
   // need to change the class of those elements to remove the expand class
-
+    $articleElements.filter(".accordian").removeClass("accordian")
+  // change the class to expand on click
+    $articleElements.addClass(accordian);
+  });
 });
