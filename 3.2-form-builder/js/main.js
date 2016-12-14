821464745d1054c55bef47c4b7f28023633d0b85
David Rangel <rangelography@gmail.com>
2016-12-14 12:49:26 -0600

    added icons to css in 3.2 assignment

--- 3.2-form-builder/js/main.js
+++ 3.2-form-builder/js/main.js
@@ -8,25 +8,25 @@ xhr.addEventListener("load", function(e){
   var formElement = document.querySelector("[data-js='form']")
   // make placeholder for outside the first forEach loop that holds everything
   var allInputDataHtml = "";
-  // make forEach loop to loop throgh inputData to identify what "type" of input
+  // make forEach loop to loop throgh responseData to identify what "type" of input
   responseData.forEach(function(inputData){
-  // make another placeholder for outside the interior forEach loop
+    // make another placeholder for outside the interior forEach loop
     var inputDataPlaceholder = "";
     // make if statement to start building "select" html. That is, if the loop sees something that has a data type that equals "select" it will start the html concatenation.
     if (inputData.type === "select"){
       // this begins the "select" html backtick IF it is the "select" data type
       inputDataPlaceholder += `<select class="selector">`;
-      // starts the next loop through the "options" in the "select" data
+        // starts the next loop through the "options" in the "select" data
         inputData.options.forEach(function(optionData){
           // continues concat'ing with the html for the "option"s in proper html
           inputDataPlaceholder += `<option label="${optionData.label}" value="${optionData.value}"</option>`;
         })
-      // the "else" statement is for all the other inputs that ARE NOT "select"
+    // the "else" statement is for all the other inputs that ARE NOT "select"
     }else {
       inputDataPlaceholder += `<input class="textInput" placeholder="  ${inputData.label}" type="${inputData.type}"
         id="${inputData.id}" icon="${inputData.icon}">`;
       // commented out the icons because I could not get them to work properly
-      // inputDataPlaceholder += `<i class="fa ${inputData.icon}" aria-hidden="true"></i>`;
+      inputDataPlaceholder += `<i class="fa ${inputData.icon} icon" aria-hidden="true"></i>`;
     }
     // concat's all inputs into the allInputData (the placeholder from above that holds everything)
     allInputDataHtml += inputDataPlaceholder;
