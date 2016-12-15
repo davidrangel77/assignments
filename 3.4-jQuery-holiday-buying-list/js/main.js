7076e7d86f1d1c25076f191646db584ee706abd7
David Rangel <rangelography@gmail.com>
2016-12-15 15:58:25 -0600

    styled form, added layered sheets effect and made js checklist

---
+++ 3.4-jQuery-holiday-buying-list/js/main.js
@@ -0,0 +1,15 @@
+// checlist for js fir gift form:
+// [] - factory call the doc to start
+// [] - factory call to $body to $ through body element
+// [] - write vars for things to ref in html elements
+// [] - add data-js in html to connect js
+// [] - make on click for ADD button
+// [] - add prevent.default to
+// [] - make a var to capture the text input
+// [] - create a new list item article with jQuery
+        // - var for button
+        // - var for article
+// [] - prepend the new article to add to the top of list
+// [] - find the cost of items
+// [] - add those items up
+// [] - insert that in the total field each time an item is added
