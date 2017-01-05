78cab293543966a04563baace7486126ef85d6e7
David Rangel <rangelography@gmail.com>
2017-01-04 19:12:31 -0600

    completed part 5 of 4.55

--- types-functions-callbacks/js/main.js
+++ types-functions-callbacks/js/main.js
@@ -109,7 +109,17 @@ console.assert(sum(5,-5) === null)
 // input numbers. You can do it using nested if statements,
 // boolean operators, or both (but not neither).
 
-
+function minimum(num1,num2,num3,num4){
+  if (num1 < num2 && num1 < num3 && num1 < num4){
+    return num1;
+  }else if (num2 < num1 && num2 < num3 && num2 < num4){
+    return num2;
+  }else if (num3 < num1 && num3 < num2 && num3 < num4){
+    return num3;
+  }else {
+    return num4;
+  }
+}
 
 console.assert(minimum(1,2,4,0) === 0)
 console.assert(minimum(1000,-2,-99,50) === -99)
@@ -122,6 +132,9 @@ console.assert(minimum(1000,-2,99,50) === -2)
 // Using logical operators, write a function that will
 // return true if either input is a string, but not
 // both or neither.
+function justOneString(){
+  
+}
 
 console.assert(justOneString('a',5) === true)
 console.assert(justOneString(6,'dotron') === true)
