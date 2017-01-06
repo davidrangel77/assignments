e5395f142b9344646afc0e5abd37f976abea22e0
David Rangel <rangelography@gmail.com>
2017-01-05 18:31:09 -0600

    got textvalue data to prepend into element...but only as text content, not as a new element

--- 5.3-gift-react/js/main.js
+++ 5.3-gift-react/js/main.js
@@ -1,20 +1,40 @@
 import React from 'react'
 
 export default React.createClass({
+  getInitialState: function() {
+    return {
+      inputValue: ''
+    };
+  },
+
+  onADDButtonClick(e) {
+    // this should return the text from the text input and append a new list item
+    e.preventDefault();
+    var currentInputValue = this.state.inputValue;
+    var textInputArray = currentInputValue.split(",");
+    var listTextElement = textInputArray[0];
+    var priceElement = textInputArray[1];
+    this.refs.listItem.prepend(listTextElement + priceElement)
+  },
+  updateInputValue: function(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  },
 
   render (){
     return (
       <main className="main">
         <h2 className="h2">GIFTS TO BUY</h2>
         <form className="items__form">
-          <input className="giftInputText" type="text"></input>
-          <input className="addButton" type="submit"></input>
-          <div className="listItems__div">
-            <article className="listItem"></article>
+          <input className="giftInputText" type="text" name="giftInput" placeholder="     name of gift, price" value={ this.state.inputValue } onChange={ this.updateInputValue }></input>
+          <input className="addButton" type="submit" name="submitItemToList" value="ADD" onClick={ this.onADDButtonClick }></input>
+          <div className="listItems__div" ref="listItems">
+            <article className="listItem" ref="listItem"></article>
             <article className="totalAmount">
               <p className="p totalTextOnly">Total</p>
               <p className="p">$</p>
-              <p className="p__totalCost"></p>
+              <p className="p__totalCost"> 0</p>
             </article>
           </div>
         </form>
