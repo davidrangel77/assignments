d8f449a1457b45d9927f3f340037c3895834d7c3
David Rangel <rangelography@gmail.com>
2017-01-19 17:44:26 -0600

    added ternary to output element and output text to setState

--- 7.3-cc-validator/js/main.js
+++ 7.3-cc-validator/js/main.js
@@ -15,8 +15,11 @@ export default React.createClass({
   },
   logOutValidity(e){
     e.preventDefault();
-    var cardValidOrNot = this.state.isCreditCardValid.toString()
-    this.setState({validityOutput:cardValidOrNot})
+    if (this.state.isCreditCardValid === false){
+      return this.setState({validityOutput:"Hell No!"})
+    } else {
+      return this.setState({validityOutput:"Good To Go!"})
+    }
   },
   render (){
     return (
@@ -24,7 +27,8 @@ export default React.createClass({
         <h3>Enter Card Number Here</h3>
         <input type="text" pattern="[0-9]{13,16}" maxLength="16" onChange={this.onCreditCardChange} required/>
         <input type="submit" onClick={this.logOutValidity}/>
-        <h3> Is this card valid? {this.state.validityOutput}</h3>
+        <h3> Is this card valid?</h3>
+        <h2 className={this.state.isCreditCardValid ? "valid" : "invalid"}>{this.state.validityOutput}</h2>
       </form>
     )
   }
