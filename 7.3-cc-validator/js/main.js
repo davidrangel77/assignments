83a22d97bd27efbea6929fc60ab6d59fd0ad5228
David Rangel <rangelography@gmail.com>
2017-01-19 17:26:40 -0600

    added display in browser to output credit card validity

--- 7.3-cc-validator/js/main.js
+++ 7.3-cc-validator/js/main.js
@@ -3,10 +3,29 @@ import Validator from 'validator'
 
 export default React.createClass({
 
+  getInitialState(){
+    return {
+      isCreditCardValid: false,
+      validityOutput: ""
+    }
+  },
+  onCreditCardChange (e){
+    var validCard = Validator.isCreditCard(e.target.value)
+    this.setState({isCreditCardValid:validCard})
+  },
+  logOutValidity(e){
+    e.preventDefault();
+    var cardValidOrNot = this.state.isCreditCardValid.toString()
+    this.setState({validityOutput:cardValidOrNot})
+  },
   render (){
     return (
-      <div></div>
+      <form>
+        <h3>Enter Card Number Here</h3>
+        <input type="text" pattern="[0-9]{13,16}" maxLength="16" onChange={this.onCreditCardChange} required/>
+        <input type="submit" onClick={this.logOutValidity}/>
+        <h3> Is this card valid? {this.state.validityOutput}</h3>
+      </form>
     )
   }
-
 })
