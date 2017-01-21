e761c10b4b611900400fa6e67676461417d8491b
David Rangel <rangelography@gmail.com>
2017-01-21 17:51:46 -0600

    styled ajax responses and reversed string response to display closer to a real chat app

--- 7.4-chat-app/js/main.js
+++ 7.4-chat-app/js/main.js
@@ -19,8 +19,9 @@ export default React.createClass({
     }
   },
   initialJsonLoaded (response){
+    var reversedJsonArray = response.reverse()
     this.setState({
-      chatJsonData: response
+      chatJsonData: reversedJsonArray
     })
   },
   onPostJsonLoaded (response){
@@ -43,18 +44,23 @@ export default React.createClass({
     e.preventDefault()
     var currentUser = this.state.userNames;
     var currentMessage = this.state.chatMessages;
-    var completeMessageDisplay = currentUser + ": " +currentMessage;
+    var completeUserDisplay = currentUser + ": ";
 
     ajax({
       url: "https://tiny-tiny.herokuapp.com/collections/davidRangel-chatApp",
       datatype: "json",
       type: "POST",
       data: {
-        completeMessageDisplay
+        currentUser: completeUserDisplay,
+        currentMessage: currentMessage
       },
       success: this.onPostJsonLoaded,
       error: this.jsonNotLoaded
     })
+  },
+  onMessagesClear(e) {
+    e.preventDefault()
+    this.setState({chatJsonData:[]})
   },
 
   render (){
@@ -62,8 +68,8 @@ export default React.createClass({
       <section className="chatAppBody">
         <div className="chatMessagesDisplay">
           {
-            this.state.chatJsonData.map((completeMessage, i)=>{
-              return <p key={i}> {completeMessage.completeMessageDisplay} </p>
+            this.state.chatJsonData.map((currentPost, i)=>{
+              return <p key={i} className="currentPost"> <span className="userNameStyle" >{currentPost.currentUser}</span> <span className="userMessageStyle">{currentPost.currentMessage}</span></p>
             })
           }
         </div>
@@ -71,6 +77,7 @@ export default React.createClass({
           <input className="userNameInput" type="text" placeholder="User Name" onChange={this.onUserInputChange}/>
           <input className="messageText" type="text" placeholder="Message Text" onChange={this.onMessageChange}/>
           <input className="submitMessage" type="submit" onClick={this.onSubmitMessage}/>
+          <input className="clearButton" type="button" value="clear" onClick={this.onMessagesClear}/>
         </form>
       </section>
     )
